dabe59d0c585cbecadfda84b7b8531a3aa54845f
AdrianRK <94287870+drAdrian9@users.noreply.github.com>
2024-12-30 20:55:55 +0700

    Files for increasing consumable slots
    Consumable Slots increased to 5 from the base 2 (for Planet or Tarot Cards)

--- Consumables Mods/IncreaseConsumableSlots.lua
+++ Consumables Mods/IncreaseConsumableSlots.lua
@@ -1,1 +1,20 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: IncreaseConsumables
+--- MOD_ID: IncreaseConsumables
+--- MOD_AUTHOR: drAdrian9
+--- MOD_DESCRIPTION: Increase Consumables to 5
 
+----------------------------------------------
+------------MOD CODE -------------------------
+
+local base_consumables = 5
+
+local originalFuncRef = get_starting_params
+function get_starting_params()
+	newTable = originalFuncRef()
+	newTable.consumable_slots = base_consumables
+	return newTable
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
