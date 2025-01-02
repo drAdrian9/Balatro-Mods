c445a06bf1a1205e36d2a3f4f7d90a9764d7eb2c
AgengCDB <107750551+AgengCDB@users.noreply.github.com>
2025-01-02 17:22:34 +0700

    Fix mult fest joker

--- JokerMods/JokerMods.lua
+++ JokerMods/JokerMods.lua
@@ -29,27 +29,8 @@ SMODS.Joker {
     end,
     calculate = function(self, card, context)
         if context.joker_main then
-            return {
-                mult_mod = card.ability.extra.mult,
-                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
-            }
-        end
-
-        -- Checks to see if it's end of round, and if context.game_over is false.
-        -- Also, not context.repetition ensures it doesn't get called during repetitions.
-        if context.end_of_round and not context.repetition and context.game_over == false and not context.blueprint then
             -- Another pseudorandom thing, randomly generates a decimal between 0 and 1, so effectively a random percentage.
             if pseudorandom('mult-fest') < G.GAME.probabilities.normal / card.ability.extra.odds then
-                -- This part plays the animation.
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        play_sound('tarot1')
-                        card.T.r = -0.2
-                        card:juice_up(0.3, 0.4)
-                        card.states.drag.is = true
-                        card.children.center.pinch.x = true
-                    end
-                }))
                 return {
                     Xmult_mod = card.ability.extra.Xmult,
                     message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } }
@@ -57,7 +38,7 @@ SMODS.Joker {
             else
                 return {
                     mult_mod = card.ability.extra.mult,
-                    message = localize { type = 'variable', key = 'no_mult', vars = {card.ability.extra.mult} }
+                    message = localize { type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult} }
                 }
             end
         end
